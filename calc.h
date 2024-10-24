5ff29ed3afb3795680e74e1071e15fb7033d0156
sommeonne <brukaliuk.serhii@lll.kpi.ua>
2024-10-24 22:58:13 +0300

    finished implementation

---
+++ calc.h
@@ -0,0 +1,14 @@
+#ifndef CALC_H
+#define CALC_H
+
+class Calc {
+public:
+
+    // The FuncA function calculates the sum of series terms based on the provided parameter.
+    // n - the number of elements to compute.
+    
+    double FuncA(int n);  
+};
+
+#endif
+
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "calc.h"
+
+int main() {
+    Calc calc;
+    int n;
+    
+    std::cout << "Enter the number of elements to sum: ";
+    std::cin >> n;
+    
+    double result = calc.FuncA(n);
+    std::cout << "Result of FuncA for n = " << n << ": " << result << std::endl;
+
+    return 0;
+}
+
